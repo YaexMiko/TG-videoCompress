77b097efe6998de8f4af7c8192b9ed7c25adb837
Yae Miko <aloksinghmains756@gmail.com>
2025-04-10 23:31:40 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,28 +1,33 @@
-#    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
-
 from decouple import config
+from telethon.tl.types import InputWebDocument
 
 try:
-    APP_ID = config("APP_ID", "28614709")
-    API_HASH = config("API_HASH", "f36fd2ee6e3d3a17c4d244ff6dc1bac8")
-    BOT_TOKEN = config("BOT_TOKEN", "7558079740:AAEVEWh1T3KP28pnPKb3bXZ1gQ0lZTpzjj0")
-    DEV = 1287276743
-    OWNER = config("OWNER", "7207533746")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL", "https://telegra.ph/file/30800c85ab730c3858b90-7f542ebcc15db4dd56.jpg")
+    APP_ID = config("APP_ID", cast=int)
+    API_HASH = config("API_HASH")
+    BOT_TOKEN = config("BOT_TOKEN")
+    DEV = 1287276743  # Replace with your DEV ID
+    OWNER = config("OWNER")
+    
+    # Watermark settings
+    WATERMARK_TEXT = config("WATERMARK_TEXT", default="Team Warlords")
+    WATERMARK_POSITION = config("WATERMARK_POSITION", default="top_right")
+    WATERMARK_FONT_SIZE = config("WATERMARK_FONT_SIZE", default=20, cast=int)
+    WATERMARK_COLOR = config("WATERMARK_COLOR", default="white")
+    
+    # Default FFmpeg code with watermark support
+    ffmpegcode = [
+        "-preset faster -c:v libx265 -s {resolution} -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' "
+        "-vf \"drawtext=text='{watermark_text}':fontsize={font_size}:fontcolor={font_color}:"
+        "x={x_pos}:y={y_pos}\" "
+        "-metadata 'title=Encoded By TG-videoCompress' "
+        "-pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"
+    ]
+    
+    THUMB = config("THUMBNAIL")
+    
+    # Notification settings
+    NOTIFICATION_CHAT_ID = config("NOTIFICATION_CHAT_ID", default=OWNER)
+    
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
